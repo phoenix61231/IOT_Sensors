54939172c64d3b1111173c117ab07f853224b448
phoenix61231 <F74041022@mail.ncku.edu.tw>
2018-01-30 18:23:06 +0800

    thread bug all fixed

--- proj_sensors.py
+++ proj_sensors.py
@@ -4,15 +4,15 @@ import Adafruit_BMP.BMP085 as BMP085
 def get_dht22(pin):
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
 
-    if humidity is not None and temperature is not None:
+    """if humidity is not None and temperature is not None:
         print('Temperature : {0:0.1f}*C\nHumidity : {1:0.1f}%'.format(temperature,humidity))
     else:
-        print('.')
+        print('.')"""
     return humidity, temperature
 	
 def get_bmp180():
     bmp180 = BMP085.BMP085()
     pressure = bmp180.read_pressure()
-    print "Air Pressure : {0:0.3f} kPa".format(pressure/1000.0)
+    #print "Air Pressure : {0:0.3f} kPa".format(pressure/1000.0)
 	
     return pressure
--- thread.py
+++ thread.py
@@ -3,6 +3,7 @@ import subprocess
 
 from datetime import datetime
 from threading import Thread
+from threading import Timer
 
 import proj_sensors
 import proj_oled
@@ -10,12 +11,13 @@ import proj_mqtt
 import proj_adc
 
 ip_address = "140.116.82.42"
-instance = "module_001"
+instance_data = "module_001_data"
+instance_status = "module_001_status"
 topic_data = "mqtt/data"
 topic_status = "mqtt/status"
 
-client_data = proj_mqtt.init_mqtt(ip_address, instance, topic_data)
-client_status = proj_mqtt.init_mqtt(ip_address, instance, topic_status)
+client_data = proj_mqtt.init_mqtt(ip_address, instance_data, topic_data)
+client_status = proj_mqtt.init_mqtt(ip_address, instance_status, topic_status)
 
 spi = proj_adc.init_adc(0, 0)
 RST = proj_oled.init_connect()
@@ -47,33 +49,31 @@ def data_thread():
 
 	
     while True:
-    	humidity_value, temperature_value = proj_sensors.get_dht22('4')	
-	pressure_value = proj_sensors.get_bmp180()
+        humidity_value, temperature_value = proj_sensors.get_dht22('4')	
+        pressure_value = proj_sensors.get_bmp180()
 
-	light_value = proj_adc.get_light(0, 2, spi)
-	uv_value = proj_adc.get_uv(1, 2, spi)
-	soil_value = proj_adc.get_soil(2, 2, spi)
+        light_value = proj_adc.get_light(0, 2, spi)
+        uv_value = proj_adc.get_uv(1, 2, spi)
+        soil_value = proj_adc.get_soil(2, 2, spi)
 	
-	data_time = str(datetime.now())
-	print(data_time + '\n')
-	
-	print "----------------------------------------"
-	time.sleep(3)
+        data_time = str(datetime.now())
+        #print(data_time)
+        print("Data Get.")
 
 def status_thread():
     global IP, CPU, MemUsage, status_time
 
     while True:
-	# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-	cmd = "hostname -I | cut -d\' \' -f1 | head --bytes -1"
-	IP = subprocess.check_output(cmd, shell = True )
-	cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
-	CPU = subprocess.check_output(cmd, shell = True )
-	cmd = "free -m | awk 'NR==2{printf \"MEM: %.2f%%\", $3*100/$2 }'"
-	MemUsage = subprocess.check_output(cmd, shell = True )
+        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        cmd = "hostname -I | cut -d\' \' -f1 | head --bytes -1"
+        IP = subprocess.check_output(cmd, shell = True )
+        cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell = True )
+        cmd = "free -m | awk 'NR==2{printf \"MEM: %.2f%%\", $3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell = True )
 
         status_time = str(datetime.now())
-        time.sleep(3)
+        print("Status Get.")
 	
 def oled_thread():
     global IP, CPU, MemUsage
@@ -81,50 +81,50 @@ def oled_thread():
     global x, top, width, height
 
     while True:
-	# Draw a black filled box to clear the image.
-	draw.rectangle((0,0,width,height), outline=0, fill=0)
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
 	
-	# Icons
-	draw.text((x, top),       unichr(61931),  font=font2, fill=255) #wifi icon
-	draw.text((x, top+15),    unichr(62171),  font=font_icon_big, fill=255) #cpu icon
+        # Icons
+        draw.text((x, top),       unichr(61931),  font=font2, fill=255) #wifi icon
+        draw.text((x, top+15),    unichr(62171),  font=font_icon_big, fill=255) 
+        #cpu icon
 	
-	draw.text((18, top),      str(IP),  font=font, fill=255) 
-	draw.text((x+22, top+12), str(CPU), font=font_text_big, fill=255) 
-	draw.text((x, top+36),    str(MemUsage),  font=font, fill=255)	
+        draw.text((18, top),      str(IP),  font=font, fill=255) 
+        draw.text((x+22, top+12), str(CPU), font=font_text_big, fill=255) 
+        draw.text((x, top+36),    str(MemUsage),  font=font, fill=255)	
 	
-	# Display image.
-	disp.image(image)
-	disp.display()
+        # Display image.
+        disp.image(image)
+        disp.display()
 	
 def send_data_thread():
     global temperature_value, humidity_value, pressure_value
     global light_value, uv_value, soil_value
-    global client_data, instance, topic_data, data_time, IP
+    global client_data, instance_data, topic_data, data_time, IP
 
     while True:
-        time.sleep(10)
-	temperature = ' / Temperature:{0:0.1f} '.format(temperature_value)
-	humidity = '/ Humidity:{0:0.1f} '.format(humidity_value)
-	light = '/ Light:{0:0.2f} '.format(light_value)
-	uv = '/ UV:{0:0.2f} '.format(uv_value)
-	soil = '/ Soil:{0:0.2f} '.format(soil_value)
-	pressure = '/ Air Pressure:{0:0.3f} '.format(pressure_value/1000.0)    
+        time.sleep(5)
+        temperature = ' / Temperature:{0:0.1f} '.format(temperature_value)
+        humidity = '/ Humidity:{0:0.1f} '.format(humidity_value)
+        light = '/ Light:{0:0.2f} '.format(light_value)
+        uv = '/ UV:{0:0.2f} '.format(uv_value)
+        soil = '/ Soil:{0:0.2f} '.format(soil_value)
+        pressure = '/ Air Pressure:{0:0.3f} '.format(pressure_value/1000.0)    
 
-    	line_data = instance + ' / ' + IP + temperature + humidity + light + uv + soil + pressure + '/ Time:' + data_time
+        line_data = instance_data + ' / ' + IP + temperature + humidity + light + uv + soil + pressure + '/ Time:' + data_time
 	
-	client_data.publish(topic_data, line_data)
-        print("----------\nSend Data.\n----------")
-        
+        client_data.publish(topic_data, line_data)
+        print("Send Data.")      
 
 def send_status_thread():
     global status_time, topic_status, client_status
-    global IP, CPU, MemUsage, instance
+    global IP, CPU, MemUsage, instance_status
 
     while True:
-        time.sleep(10)
-	line_status = instance + ' / ' + IP + ' / CPU:' + CPU + ' / ' + MemUsage + ' / ' + status_time
-	client_status.publish(topic_status, line_status)
-        print("----------\nSend Status.\n----------")
+        time.sleep(5)
+        line_status = instance_status + ' / ' + IP + ' / CPU:' + CPU + ' / ' + MemUsage + ' / ' + status_time
+        client_status.publish(topic_status, line_status)
+        print("Send Status.")
         
 
 
@@ -142,4 +142,3 @@ send_data_t.start()
 
 send_status_t = Thread(target=send_status_thread, name="send_status_t")
 send_status_t.start()
-
