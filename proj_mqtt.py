fbdbd9920c308d2f63835a033e46f490d4855999
phoenix61231 <kimi851231@gmail.com>
2018-02-07 15:20:48 +0000

    add some commit and wifi detect and bug fix

--- proj_mqtt.py
+++ proj_mqtt.py
@@ -1,12 +1,13 @@
 import paho.mqtt.client as mqtt
 import thread
 
+
 def on_message(client, userdata, message):
     print(str(message.payload.decode("utf-8") + " (" + message.topic + ")"))
 
 def init_mqtt(broker_address, instance, topic):
     default_address = "140.116.82.42"
-    default_topic = "mqtt/demo"
+    default_topic = "mqtt/data"
 	
     # Modify the broker ip here, default will be uscclab server	
     if broker_address is None:
@@ -16,16 +17,16 @@ def init_mqtt(broker_address, instance, topic):
     print("creating new instance")	
     client = mqtt.Client(instance)
     client.on_message = on_message
-
+    
     print("connecting to broker at " + broker_address)
-    client.connect(broker_address)  # connect to broker
+    client.connect_async(broker_address, keepalive=15)  # connect to broker
     #if connect fail fix
 
     # Enter the topic to subscribe here, web default is "mqtt/demo"
     if topic is None:
 	topic = default_topic		
 
-    client.loop_start()  # start the loop
+    client.loop_start() # start the loop
     print("Subscribing to topic : " + topic)
     client.subscribe(topic)
 	
--- thread.py
+++ thread.py
@@ -5,29 +5,25 @@ from datetime import datetime
 from threading import Thread
 from threading import Timer
 
+from wifi import Cell
+
 import proj_sensors
 import proj_oled
 import proj_mqtt
 import proj_adc
 
+#the mqtt client setting
 ip_address = "140.116.82.42"
 instance_data = "module_001_data"
 instance_status = "module_001_status"
 topic_data = "mqtt/data"
 topic_status = "mqtt/status"
 
+#Initital the mqtt client
 client_data = proj_mqtt.init_mqtt(ip_address, instance_data, topic_data)
 client_status = proj_mqtt.init_mqtt(ip_address, instance_status, topic_status)
 
-spi = proj_adc.init_adc(0, 0)
-RST = proj_oled.init_connect()
-font, font2, font_icon_big, font_text_big = proj_oled.load_font()
-
-disp = proj_oled.init_disp(RST)
-width, height = proj_oled.def_size(disp)
-x, top = proj_oled.draw_shape(height)
-image, draw = proj_oled.create_blank(width, height)
-
+#Decline the global variable
 humidity_value = 0.0
 temperature_value = 0.0
 pressure_value = 0.0
@@ -42,10 +38,17 @@ IP = ''
 CPU = ''
 MemUsage = ''
 
+wifi_signal = ''
+wifi_quality = ''
+
+#define the data thread
+#for initial and getting data
 def data_thread():
-    global spi, data_time
+    global data_time
     global humidity_value, temperature_value, pressure_value
     global light_value, uv_value, soil_value
+	
+    spi = proj_adc.init_adc(0, 0)
     
     while True:
         humidity_value, temperature_value = proj_sensors.get_dht22('4')	
@@ -58,7 +61,10 @@ def data_thread():
         data_time = str(datetime.now())
         print(data_time)
         print("--------------------")
+        time.sleep(10)
 
+#define the status thread
+#for initial and getting status
 def status_thread():
     global IP, CPU, MemUsage, status_time
 
@@ -72,11 +78,21 @@ def status_thread():
         MemUsage = subprocess.check_output(cmd, shell = True )
 
         status_time = str(datetime.now())
+        time.sleep(5)
 	
+
+#define the oled display thread
+#for initial and oled display
 def oled_thread():
     global IP, CPU, MemUsage
-    global draw, disp, image, font, font2, font_icon_big, font_text_big
-    global x, top, width, height
+
+    RST = proj_oled.init_connect()
+    font, font2, font_icon_big, font_text_big = proj_oled.load_font()
+
+    disp = proj_oled.init_disp(RST)
+    width, height = proj_oled.def_size(disp)
+    x, top = proj_oled.draw_shape(height)
+    image, draw = proj_oled.create_blank(width, height)        
 
     while True:
         # Draw a black filled box to clear the image.
@@ -94,14 +110,16 @@ def oled_thread():
         # Display image.
         disp.image(image)
         disp.display()
-	
+
+#define send data thread
+#for the sending data operation with publish error detect
 def send_data_thread():
     global temperature_value, humidity_value, pressure_value
     global light_value, uv_value, soil_value
     global client_data, instance_data, topic_data, data_time, IP
 
     while True:
-        time.sleep(30)
+        time.sleep(60)
         temperature = ' / Temperature:{0:0.1f} '.format(temperature_value)
         humidity = '/ Humidity:{0:0.1f} '.format(humidity_value)
         light = '/ Light:{0:0.2f} '.format(light_value)
@@ -111,17 +129,55 @@ def send_data_thread():
 
         line_data = instance_data + ' / ' + IP + temperature + humidity + light + uv + soil + pressure + '/ Time:' + data_time
 	
-        client_data.publish(topic_data, line_data)
-    
+        info = client_data.publish(topic_data, line_data)
+
+        if info.rc is not 0:
+            #save to .txt
+            with open('data.txt', 'a') as file:
+                file.write(line_data + '\n')
+                print("Save to data.txt file.")
+            
+
+#define send status thread
+#for the sending data operation with publish error detect
 def send_status_thread():
     global status_time, topic_status, client_status
     global IP, CPU, MemUsage, instance_status
 
     while True:
         time.sleep(30)
-        line_status = instance_status + ' / ' + IP + ' / CPU:' + CPU + ' / ' + MemUsage + ' / ' + status_time
-        client_status.publish(topic_status, line_status)
+        line_status = instance_status + ' / ' + IP + ' / CPU:' + CPU + ' / ' + MemUsage + ' / ' + wifi_signal + ' / ' + str(wifi_quality) + ' / ' +status_time
+        info = client_status.publish(topic_status, line_status)
         
+        if info.rc is not 0:
+            #save to .txt
+            with open('status.txt', 'a') as file:
+                file.write(line_status + '\n')
+                print("Save to status.txt file.")
+            
+
+#define wifi connection detect thread
+#for the wifi connection signal and quality detect
+def wifi_connection_detect_thread():
+	
+    global wifi_quality, wifi_signal
+
+    while True:
+        #wifi signal & quality
+	cell = Cell.all('wlan0')[0]
+
+        if cell is not None:
+	    wifi_signal = str(cell.signal) + ' dBm'
+        
+            quality = cell.quality
+            front = float(quality[1]) + 10.0*float(quality[0])
+            rear = float(quality[4]) + 10.0*float(quality[3]) 
+            wifi_quality = '{0:0.2f} %'.format(100.0*(front/rear))
+        else:
+            wifi_signal = 'Wifi Connection Fail'
+            wifi_quality = 'Wifi Connection Fail'
+
+	time.sleep(5)	
 
 data_t = Thread(target=data_thread, name="data_t")
 data_t.start()
@@ -130,10 +186,13 @@ status_t = Thread(target=status_thread, name="status_t")
 status_t.start()
 
 oled_t = Thread(target=oled_thread, name="oled_t")
-oled_t.start()
+#oled_t.start()
 
 send_data_t = Thread(target=send_data_thread, name="send_data_t")
 send_data_t.start()
 
 send_status_t = Thread(target=send_status_thread, name="send_status_t")
 send_status_t.start()
+
+wifi_connection_detect_t = Thread(target=wifi_connection_detect_thread, name="connection_detect_t")
+wifi_connection_detect_t.start()
